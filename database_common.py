d1dadbe1eccfdb8652e5d9d012a3d3e69309eed5
AttilaMuller <muller.attila.aron@gmail.com>
2018-02-24 23:36:51 +0100

    getting ready to heroku db

--- database_common.py
+++ database_common.py
@@ -18,8 +18,7 @@ def get_connection_string():
 
 def open_database():
     try:
-        connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        connection = get_connection_string()
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
